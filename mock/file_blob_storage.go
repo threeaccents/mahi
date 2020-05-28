c9ee1dccc098ab6194063fb0e126c446c15fa96b
threeaccents <rodrigo@threeaccents.com>
2020-05-28 15:15:42 -0600

    update mock

--- mock/file_blob_storage.go
+++ mock/file_blob_storage.go
@@ -16,3 +16,7 @@ func (s *FileBlobStorage) Upload(ctx context.Context, bucket string, blob *mahi.
 func (s *FileBlobStorage) CreateBucket(ctx context.Context, bucket string) error {
 	return nil
 }
+
+func (s *FileBlobStorage) FileBlob(ctx context.Context, bucket, id, tempDir string) (*mahi.FileBlob, error) {
+	return &mahi.FileBlob{}, nil
+}
