99064d6e4f4e0eb2e1ab26d6ec873e9ed246e4f1
threeaccents <rodrigo@threeaccents.com>
2020-06-16 08:12:11 -0600

    npm

--- cmd/mahiadmin/main.go
+++ cmd/mahiadmin/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	$END$
+	staticFs := http.FileServer(http.Dir("./static/www/assets"))
+	http.Handle("/static/", http.StripPrefix("/static", staticFs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/www/index.html")
+	})
+
+	log.Println("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
