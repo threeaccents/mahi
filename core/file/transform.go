a4900dea99961d0b7ee37e9820578ef23504e094
threeaccents <rodrigo@threeaccents.com>
2020-09-02 15:47:03 -0600

    fuck this piece of shit

--- core/file/transform.go
+++ core/file/transform.go
@@ -138,14 +138,9 @@ func convertBimgOptions(opts mahi.TransformationOption) bimg.Options {
 		Width:       opts.Width,
 		Height:      opts.Height,
 		Gravity:     bimg.GravitySmart, // smart crop
-		Type:        fileType,
 		Crop:        opts.Crop,
-		Rotate:      bimg.Angle(opts.Rotate),
-		Flip:        opts.Flip,
-		Flop:        opts.Flop,
 		Quality:     opts.Quality,
 		Compression: opts.Compression,
-		Zoom:        opts.Zoom,
 	}
 
 	if opts.BW {
