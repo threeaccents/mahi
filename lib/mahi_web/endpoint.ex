0de4b1e05cb1a89f4a567fab75dded4a56412312
threeaccents <rodrigo@threeaccents.com>
2021-07-01 13:42:08 -0600

    add cors

--- lib/mahi_web/endpoint.ex
+++ lib/mahi_web/endpoint.ex
@@ -47,6 +47,7 @@ defmodule MahiWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug CORSPlug
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
